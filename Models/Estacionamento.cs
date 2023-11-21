242d201097c0dedcab829bb7069f08377d541be0
Renata Garcia <renata.cavalcanti.garcia@gmail.com>
2023-11-21 17:58:14 -0300

    Adiciona Arquivos

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,92 @@
+﻿namespace FastPark.Models
+{
+    //classe Estacionamento --> de acordo com o diagrama
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.Clear();
+            ExibirTituloDaOpcao("Cadastro de veículos");
+            Console.WriteLine("Digite a placa do veículo:");
+            string placa = Console.ReadLine()!;
+            veiculos.Add(placa);
+            Console.WriteLine($"\nA placa {placa} foi registrada com sucesso!\n");
+
+            Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+           
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.Clear();
+            ExibirTituloDaOpcao("Saída de veículos");
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine()!;
+
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                string InputHora = Console.ReadLine()!;
+                decimal horas = Convert.ToDecimal(InputHora);
+
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                veiculos.Remove(placa.ToUpper());
+
+                Console.WriteLine($"O veículo de placa{placa} foi removido");
+                Console.WriteLine($"o valor total foi {valorTotal}");
+                Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            Console.Clear();
+            ExibirTituloDaOpcao("Veículos estacionados");
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Count != 0)
+            {
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine($"Os veículos estacionados são: {veiculo}");
+                }
+                Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+
+        void ExibirTituloDaOpcao(string titulo)
+        {
+            int quantidadeDeLetras = titulo.Length;
+            string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+            Console.WriteLine(asteriscos);
+            Console.WriteLine(titulo);
+            Console.WriteLine(asteriscos + "\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿//Fast Park
+using FastPark.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string mensagemDeBoasVindas = "\nOlá, boas vindas ao Fast Park";
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+█▀▀ ▄▀█ █▀ ▀█▀   █▀█ ▄▀█ █▀█ █▄▀
+█▀░ █▀█ ▄█ ░█░   █▀▀ █▀█ █▀▄ █░█");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+    //Input usuário do preço inicial
+    
+    ExibirTituloDaOpcao("Registro dos Preços");
+    Console.WriteLine("Digite o preço inicial: R$");
+    precoInicial = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine($"\nO valor R${precoInicial} foi registrado com sucesso!");
+
+    //Input usuário do preço por hora
+    Console.WriteLine("\nDigite o preço por hora: R$");
+    precoPorHora = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine($"\nO valor R${precoPorHora} foi registrado com sucesso!");
+
+    Console.WriteLine("\nDigite uma tecla para votar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new(precoInicial, precoPorHora);
+
+void ExibirMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para cadastrar um veículo");
+    Console.WriteLine("Digite 2 para remover um veículo");
+    Console.WriteLine("Digite 3 para exibir os veículos estacionados");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            es.AdicionarVeiculo();
+            ExibirMenu();
+            break;
+        case 2:
+            es.RemoverVeiculo();
+            ExibirMenu();
+            break;
+        case 3:
+            es.ListarVeiculos();
+            ExibirMenu();
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            ExibirMenu();
+            break;
+    }
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+ExibirMenu();
